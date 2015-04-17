a0fb0c9555dcdb9bd9587593c483fecfe7ed0e10
Won-beom,Choi <onebc0909@naver.com>
2015-04-17 18:26:01 +0900

    [Using DXL & Backtracking] Solving sudoku puzzle

---
+++ src/SudokuBack.cpp
@@ -0,0 +1,113 @@
+// A Backtracking program  in C++ to solve Sudoku problem 
+#include <stdio.h> 
+#include <ctime>
+// UNASSIGNED is used for empty cells in sudoku grid 
+#define UNASSIGNED 0 
+// N is used for size of Sudoku grid. Size will be NxN 
+#define N 9 
+
+int count = 0;
+// This function finds an entry in grid that is still unassigned 
+bool FindUnassignedLocation(int grid[N][N], int &row, int &col);  
+// Checks whether it will be legal to assign num to the given row,col 
+bool isSafe(int grid[N][N], int row, int col, int num);  
+/* Takes a partially filled-in grid and attempts to assign values to   all unassigned locations in such a way to meet the requirements   for Sudoku solution (non-duplication across rows, columns, and boxes) */ 
+void printGrid(int grid[N][N]);
+
+bool SolveSudoku(int grid[N][N]) {
+	int row, col;       
+	// If there is no unassigned location, we are done     
+	if (!FindUnassignedLocation(grid, row, col))
+	{
+		printGrid(grid);
+		count++;
+		return true; // success!
+	}
+	// consider digits 1 to 9     
+	for (int num = 1; num <= 9; num++)     {         
+		// if looks promising         
+		if (isSafe(grid, row, col, num))         {             
+			// make tentative assignment             
+			grid[row][col] = num;               
+			// return, if success, yay!             
+			if (SolveSudoku(grid))
+				return true;
+			// failure, unmake & try again             
+			grid[row][col] = UNASSIGNED;         
+		}     
+	}     
+	return false; // this triggers backtracking 
+}  
+/* Searches the grid to find an entry that is still unassigned. If    found, the reference parameters row, col will be set the location    that is unassigned, and true is returned. If no unassigned entries    remain, false is returned. */ 
+bool FindUnassignedLocation(int grid[N][N], int &row, int &col) {     
+	for (row = 0; row < N; row++)         
+		for (col = 0; col < N; col++)             
+			if (grid[row][col] == UNASSIGNED)                 
+				return true;     
+	return false; 
+}
+/* Returns a boolean which indicates whether any assigned entry    in the specified row matches the given number. */ 
+bool UsedInRow(int grid[N][N], int row, int num) {     
+	for (int col = 0; col < N; col++)         
+		if (grid[row][col] == num)             
+			return true;     
+	return false; 
+}  
+/* Returns a boolean which indicates whether any assigned entry    in the specified column matches the given number. */ 
+bool UsedInCol(int grid[N][N], int col, int num) {     
+	for (int row = 0; row < N; row++)         
+		if (grid[row][col] == num)             
+			return true;     
+	return false; 
+}  
+/* Returns a boolean which indicates whether any assigned entry    within the specified 3x3 box matches the given number. */ 
+bool UsedInBox(int grid[N][N], int boxStartRow, int boxStartCol, int num) {     
+	for (int row = 0; row < 3; row++)         
+		for (int col = 0; col < 3; col++)             
+			if (grid[row+boxStartRow][col+boxStartCol] == num)                 
+				return true;     
+	return false; 
+}  
+/* Returns a boolean which indicates whether it will be legal to assign    num to the given row,col location. */ 
+bool isSafe(int grid[N][N], int row, int col, int num) {     
+	/* Check if 'num' is not already placed in current row,        current column and current 3x3 box */    
+	return !UsedInRow(grid, row, num) && !UsedInCol(grid, col, num) && !UsedInBox(grid, row - row%3 , col - col%3, num); 
+}  
+/* A utility function to print grid  */ 
+void printGrid(int grid[N][N]) {     
+	for (int row = 0; row < N; row++)     {        
+		for (int col = 0; col < N; col++)              
+			printf("%2d", grid[row][col]);         
+		printf("\n");     
+	} 
+	printf("\n");
+}  
+/* Driver Program to test above functions */ 
+int main() {     
+	// 0 means unassigned cells     
+	int grid[N][N];     
+	time_t startTime;
+	int testCase;
+	FILE* inFile = fopen("sudoku.txt", "r");
+	FILE* out = fopen("output.txt", "w");
+	fscanf(inFile, "%d ", &testCase);
+	while ( testCase-- )
+	{
+		for(int i=0; i<N; i++)
+			for(int j=0; j<N; j++)
+				fscanf(inFile, "%d ", &grid[i][j]);
+		for (int i = 0; i < 1; i++)
+		{
+			startTime = clock();
+			SolveSudoku(grid);
+			int end = clock();
+			fprintf(out, "%d\n", end - startTime);
+			printf("runing time : %d\n", end - startTime);
+		}
+	}
+	printf("success %d solve\n", count);
+
+	fclose(inFile);
+	fclose(out);
+	return 0;
+}
